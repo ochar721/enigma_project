c43780e0db8c66d5d5340e340342fd47424361b7
Olivia Charles <ocharles92@gmail.com>
2021-01-16 21:30:42 -0500

    Created method for split_number

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,38 +1,46 @@
 class Enigma
-    attr_reader :enigma_cipher
+    attr_reader
 
-  def initialize(message, random_number, date)
-      @message         = message
-      @random_number   = random_number
-      @date            = date
+  def initialize
+      # @message         = message
+      # @random_number   = random_number
+      # # @date            = date
   end
 
   def create_alphabet
     alphabet = ("a".."z").to_a << " "
   end
 
-  def encrypt(message, shift)
+  def encrypt(message, shift, date)
     return_message = ""
     message.each_char do |letter|
-      if create_alphabet.include? (letter.downcase)
-        shift.times {letter = letter.next}
+      if !create_alphabet.include?(letter.downcase)
+        return_message += letter
+      else
+        old_index = create_alphabet.index(letter)
+        new_index = (old_index + shift + date) % 27
+        return_message += create_alphabet[new_index]
       end
-    return_message << letter[-1]
     end
-   return_message
-  end
-
-  def double_encrypt(message, random_number, date)
-    return_message = ""
-    message.each_char do |letter|
-      if create_alphabet.include? (letter.downcase)
-        (random_number + date).times {letter = letter.next}
-      end
-     return_message << letter[-1]
-     end
     return_message
   end
 
+  def generate_random_number
+    rand.to_s[2..6]
+  end
+
+  def split_number
+    number  = generate_random_number
+    number1 = number[0..1].to_i
+    number2 = number[1..2].to_i
+    number3 = number[2..3].to_i
+    number4 = number[3..4].to_i
+  end
+
+  # def generate_date
+  #
+  #
+  # end
 end
   #
   #   def ask(question)
