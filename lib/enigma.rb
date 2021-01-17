982f2ecb07e248d9a40afb48776943010edae755
Olivia Charles <ocharles92@gmail.com>
2021-01-16 19:16:35 -0500

    Creating a mini ceasar cipher to help me understand

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,44 @@
 class Enigma
+    attr_reader :enigma_cipher
+
+  def initialize(message, random_number, date)
+      @message         = message
+      @random_number   = random_number
+      @date            = date
+  end
+
+  def create_alphabet
+    alphabet = ("a".."z").to_a << " "
+  end
+
+  def encrypt(message, shift)
+    return_message = ""
+    message.each_char do |letter|
+      if create_alphabet.include? (letter.downcase)
+        shift.times {letter = letter.next}
+      end
+    return_message << letter[-1]
+    end
+   return_message
+  end
+
+  def double_encrypt(message, random_number, date)
+    return_message = ""
+    message.each_char do |letter|
+      if create_alphabet.include? (letter.downcase)
+        (random_number + date).times {letter = letter.next}
+      end
+     return_message << letter[-1]
+     end
+    return_message
+  end
 
 end
+  #
+  #   def ask(question)
+  #       print question
+  #       answer= gets.chomp
+  #   end
+  # answer = ask("What would you like to encrypt? ")
+  # puts encrypt(answer, 5)
+  # end
--- lib/keys.rb
+++ lib/keys.rb
@@ -1,11 +1,17 @@
 class Keys
-  def initialize(random_number)
-    @random_number = random_number.to_s
-  end
 
-  def generate_key
-    @random_number.rand(5)
-  end
+  # attr_reader :keys
+  #
+  # def initialize(keys)
+  #   @A = keys[:A]
+  #   @B = keys[:B]
+  #   @C = keys[:C]
+  #   @D = keys[:D]
+  # end
+  #
+  # def generate_key
+  #   @random_number.rand(5)
+  # end
 end
 #   The keys are created by generating a random five digit number, like 02715, and splitting it up like so:
 #
