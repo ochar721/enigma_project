f832b1eff06650aab943b3f6b57f41dc228b86fd
Olivia Charles <ocharles92@gmail.com>
2021-01-16 21:30:26 -0500

    Created test for split number

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -3,26 +3,36 @@ require "./lib/enigma"
 
 class EnigmaTest < Minitest::Test
   def test_it_exists
-    enigma = Enigma.new("Olivia", 5, 2)
+    enigma = Enigma.new
     assert_instance_of Enigma, enigma
   end
 
   def test_it_can_create_an_alphabet
-    enigma = Enigma.new("Olivia", 5, 2)
+    enigma = Enigma.new
     expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
     assert_equal expected, enigma.create_alphabet
   end
 
   def test_it_can_encrypt_simple_shift
-    skip
-    enigma = Enigma.new("hello", 5)
-    assert_equal "mjqqt", enigma.encrypt("hello", 5)
-    enigma1 = Enigma.new("olivia", 5)
-    assert_equal "tqn nf", enigma1.encrypt("olivia", 5)
+
+    enigma = Enigma.new
+    assert_equal "mjqqt", enigma.encrypt("hello", 5, 0)
+    enigma1 = Enigma.new
+    assert_equal "tqn nf", enigma1.encrypt("olivia", 5, 0)
   end
 
   def test_it_can_encrypt_double_shift
-    enigma1 = Enigma.new("hello", 5, 2)
-    assert_equal "olssv", enigma1.double_encrypt("hello", 5, 2)
+    enigma = Enigma.new
+    assert_equal "olssv", enigma.encrypt("hello", 5, 2)
+  end
+
+  def test_generate_five_random_number
+    enigma = Enigma.new
+    assert_equal 5, enigma.generate_random_number.to_s.length
+  end
+
+  def test__split_number
+    enigma = Enigma.new
+    assert_equal 2, enigma.split_number.to_s.length
   end
 end
